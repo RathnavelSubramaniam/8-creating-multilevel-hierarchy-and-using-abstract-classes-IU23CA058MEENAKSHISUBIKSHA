1fa94992ab7f17f63f44c57ca68abaa7e26cef13
IU23CA058MEENAKSHISUBIKSHA <142492311+IU23CA058MEENAKSHISUBIKSHA@users.noreply.github.com>
2024-03-06 10:50:51 +0530

    Update Practical8.java

--- Practical8.java
+++ Practical8.java
@@ -1,26 +1,57 @@
 // Animal.java
 public abstract class Animal {
-// Basic attributes and methods including abstract sound method
-}
+    protected String species;
+    protected int age;
 
-// Mammal.java
-public class Mammal extends Animal {
-// Additional attributes and methods
+    public Animal(Sring species,int age){
+        this.species=species;
+        thisa.age=age;
+    }
+    public abstract void sound();
+
+    @Override
+    public String toString{
+        return "Species:"+species+",Age:"+age;
+    }
+    @Override
+    public boolean equals(Object obj){
+
+    }
 }
 
 // Bird.java
 public class Bird extends Animal {
-// Additional attributes and methods
-}
+ private String featherColor;
 
+ public Brid(String species,int age,String featherColor){
+ super(species,age);
+ this.featherColor=feathercolor;
+
+}
+public void sound(){
+    System.out.println("The brid sound");
+}
+}
 // Parrot.java
 public class Parrot extends Bird {
-// Additional characteristics specific to Parrot
+private boolean canSpeack;
+public Parrot(String species,int age,String featherColor,boolean canSpeack){
+    super()
+
+}
+else{
+    System.out.println("Parrod sound")
+}
 }
 
 // Main.java
 public class Main {
 public static void main(String[] args) {
-// Instantiate and demonstrate object class methods
+  Mummal line=new Mummal("Lion",5,"Grasslands");
+    Brid eagle=new Brid("Eagle",3,"Brown");
+    Parrot talkingParrot=new Parrot("Talking Parrot",2,"Green",true);
+
+    System.out.println("Are lion and eagle equal?"+lion.equals(eagele));
+    System.out.println("Are lion and lion equal?"+lion.equals(new Mammal("Lion",5,"Grasslands")));
 }
 }
